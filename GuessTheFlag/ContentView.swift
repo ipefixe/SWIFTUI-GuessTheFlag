e70e9e3eafb93e70decefb61ce8dd58fd9ceb5ba
Ipefixe <ipefixe.fr@gmail.com>
2021-01-24 22:30:31 +0100

    First commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Kevin Boulala on 23/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreContentMessage = ""
+    @State private var score = 0
+    
+    var body: some View {
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .fontWeight(.black)
+                        .font(.largeTitle)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        flagTapped(number)
+                    }, label: {
+                        Image(self.countries[number])
+                            .border(Color.black, width: 1)
+                            .shadow(radius: 4)
+                    })
+                }
+                
+                Text("Your score is \(score)")
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+        }
+        
+        .alert(isPresented: $showingScore, content: {
+            Alert(title: Text(scoreTitle), message: Text(scoreContentMessage), dismissButton: .default(Text("Continue"), action: {
+                askQuestion()
+            }))
+        })
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            scoreContentMessage = "Correct! Your score is \(score)"
+        } else {
+            scoreTitle = "Wrong"
+            scoreContentMessage = "Wrong! That's the flag of \(countries[number])"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kevin Boulala on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
